c84dffcc86e8780bdd044123d5f13d66f3fc536a
Jamesty7 <jamestysoto@gmail.com>
2022-11-27 23:45:26 +0100

    - add functions to login and fetch data from instagram

--- inspector.py
+++ inspector.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from lxml import etree
+from time import sleep
 
 def get_github_info(url):
     driver = webdriver.Chrome()
@@ -23,10 +25,105 @@ def get_github_info(url):
 
     #fork_icon = github_soup.find_all('svg', {"class":"octicon octicon-repo-forked mr-2"})[1]
     #forks = fork_icon.find_next_sibling("strong").text.strip()
+    return {
+        'repo_name': repo_name,
+        'author': author,
+        'issues': issues,
+        'pull_requests': pull_requests,
+        'stars': stars,
+        'watchers': watchers,
+        'forks': forks,
+        'languages': languages
+    }
 
-    print("{} by {}: {} issues, {} PRs, {} stars, {} watchers, {} forks, languages {}".format(repo_name, author, issues, pull_requests, stars, watchers, forks, languages))
+def ig_login(driver, username, password):
+    sleep(2)
+    driver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
+    sleep(3)
+
+    cookie_button = driver.find_element(By.XPATH, "//div[@role='dialog']//button[@tabindex='0']")
+    cookie_button.click()
+    username_el = driver.find_element(By.NAME, 'username')
+    username_el.send_keys(username)
+    password_el = driver.find_element(By.NAME, 'password')
+    password_el.send_keys(password)
+
+    #instead of searching for the Button (Log In) you can simply press enter when you already selected the password or the username input element.
+    submit = driver.find_element(By.TAG_NAME, 'form')
+    sleep(10)
+    submit.submit()
+    sleep(10)
+    # not now
+    save_login_info_button= driver.find_element(By.XPATH, "//button[text()='Not Now']")
+    save_login_info_button.click()
+    sleep(2)
+
+def scroll(driver, timeout):
+    scroll_pause_time = timeout
+
+    # Get scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # Wait to load page
+    sleep(scroll_pause_time)
+
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        # If heights are the same it will exit the function
+        return
+    last_height = new_height
 
 
+def ig_get_posts_for_hashtag(driver, hashtag, min_images_required):
+    driver.get('https://www.instagram.com/explore/tags/' + hashtag) # Load the page at the given url with the #Hashtag
+    sleep(15)
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") # Scrolls to the ZERO position (horizontal)
 
-get_github_info("https://github.com/CodeYourFuture/node-challenge-chat-server")
+    posts = set()
+    while len(posts) < min_images_required:
+        old_count = len(posts)
+        data = BeautifulSoup(driver.page_source, 'html.parser')
+        dom = etree.HTML(str(data))
+        links = dom.xpath('//a[starts-with(@href, "/p/")]')
+        for link in links:
+            page_url = link.attrib['href']
+            posts.add(page_url)
+        scroll(driver, 3)
+        if old_count == len(posts): # No new images loaded - exit
+            break
+    print(len(posts), " Images Found")
+    return list(posts)
 
+def ig_get_post_info(driver, post_url):
+    driver.get('https://www.instagram.com' + post_url) # Load the page at the given url with the #Hashtag
+    sleep(10)
+    cookie_button = driver.find_element(By.XPATH, "//div[@role='dialog']//button[@tabindex='0']")
+    cookie_button.click()
+    content = driver.page_source.encode("utf-8").strip()
+    data = BeautifulSoup(content, 'html.parser')
+    dom = etree.HTML(str(data))
+    author = dom.xpath('//header//a')[0].text
+    post_text = dom.xpath('//h2/following-sibling::div//span/text()')
+    print(post_text)
+    #print(etree.tostring(post_text, pretty_print = True))
+
+    post_tags = dom.xpath('//a[starts-with(text(), "#")]')
+    tags = []
+    for item in post_tags:
+        tags.append(item.text)
+    print("Author {}, Text {}, Tags {} ".format(author, post_text, tags))
+        
+
+repo_data = get_github_info("https://github.com/CodeYourFuture/node-challenge-chat-server")
+print(repo_data)
+
+# driver = webdriver.Chrome()
+# ig_login(driver, "username", "password")
+# hashtag = 'disneymerch' # This assigns the #hashtag that will be searched for in Instagram
+# posts = ig_get_posts_for_hashtag(driver, hashtag, 20)
+# print(posts)
+# ig_get_post_info(driver, '/p/CleF8YIuiHH/')
